68aa77cc9edfd87f6c81d7030ffd8a610d6a6114
jecamargo10 <javier_22ca@hotmail.com>
2017-03-29 20:52:50 -0500

    Update main.js

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ import {Canchas} from '../imports/api/canchas.js'
 import {Usuarios} from '../imports/api/usuarios.js'
 import {Partidos} from '../imports/api/partidos.js'
 
-
+//Deberias aca definir los meteor methods asociados a la base de datos y les añades aca los nombres que llamaras desde el app.js
 Meteor.startup(() => {
   // code to run on server at startup
 });
